3a5c598abfa6532ec3bf2e453610b5695c776730
vasanth-123 <95040104+vasanth-123@users.noreply.github.com>
2022-11-24 20:37:06 +0530

    Add files via upload

---
+++ html/javascript/java.js
@@ -0,0 +1,70 @@
+//number
+//string
+//arrays
+//variable "var"
+/*var a=10;
+var names="tamil";
+var name1="vasanth";
+
+//arrays
+//var animal=["tiger","dog","lion"];
+var no=[5,45,45,67,89,"vasanth"];
+no[7]="hii";//add value in  no
+no.push("vsa");// add value  didn't gve position
+//alert(no.length);// length strart from 1
+//alert(no.reverse());
+//alert(animal[0]);//[]position
+//loops
+//console "fn+f12" inspect page
+//console.log("hi");
+/*for(var i=0;i<5;i++){
+    console.log(i);
+
+}*/
+/*var i=10;
+while(i<15){
+    
+    console.log(i);
+    i++;
+}*/
+/*var no3=[1,2,3,4,5,6,7];
+no3.forEach(function(no4){
+    console.log(no4);
+}
+);*/
+//contition
+/*var a1=10
+var b1=15
+var c1=15
+if(a1==b1 && b1==c1){
+ console.log("true");
+}
+else{
+    console.log("false");
+}
+*/
+//switch
+//var clr="red";
+/*var clr="";
+switch(clr){
+    case"red":console.log("i like red");
+    break;
+    case"blue":console.log("its okat");
+    break;
+    //default:console.log("choose any clr");
+    default:alert("choose any clr");
+}*/
+//objects constractor
+/*var animal=new Object
+animal.name = "tiger";
+animal.type="killer";
+animal.colo="yellow";
+console.log(animal.type);*/
+/*function animal(nam,type,colour){
+    this.name=nam;
+    this.type=type;
+    this.colour=colour;
+}
+var tiger=new animal("dog","wow","white")
+var dog=new animal("dog","wow","white")
+console.log(tiger)*/
